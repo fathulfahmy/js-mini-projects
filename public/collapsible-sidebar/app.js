ebb4d808b0be9af79e20d9396d406ac8fad0ea5b
fathulfahmy <fathulfmy@gmail.com>
2023-10-11 17:41:00 +0800

    feat: add collapsible sidebar

---
+++ public/collapsible-sidebar/app.js
@@ -0,0 +1,11 @@
+// DOM
+const menuBtn = document.querySelector("#menu-btn");
+const navLinks = document.querySelector("#nav-links");
+// toggle
+menuBtn.addEventListener("click", () => {
+	navLinks.classList.toggle("w-0");
+	navLinks.classList.toggle("h-0");
+	navLinks.classList.toggle("w-1/3");
+	navLinks.classList.toggle("h-screen");
+	navLinks.classList.toggle("overflow-hidden");
+});
