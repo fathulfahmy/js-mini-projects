418971037033d138c4edfa8f377a2a9982e4036f
fathulfahmy <fathulfmy@gmail.com>
2023-10-12 09:44:44 +0800

    feat: responsive collapsible sidebar

--- public/collapsible-sidebar/app.js
+++ public/collapsible-sidebar/app.js
@@ -2,10 +2,11 @@
 const menuBtn = document.querySelector("#menu-btn");
 const navLinks = document.querySelector("#nav-links");
 // toggle
-menuBtn.addEventListener("click", function() {
+menuBtn.addEventListener("click", function () {
 	navLinks.classList.toggle("w-0");
 	navLinks.classList.toggle("h-0");
-	navLinks.classList.toggle("w-1/3");
-	navLinks.classList.toggle("h-screen");
 	navLinks.classList.toggle("overflow-hidden");
+	navLinks.classList.toggle("w-52");
+	navLinks.classList.toggle("sm:w-72");
+	navLinks.classList.toggle("h-screen");
 });
